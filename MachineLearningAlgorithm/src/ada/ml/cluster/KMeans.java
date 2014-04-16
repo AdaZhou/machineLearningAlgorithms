c58c09fd1be1cf62a6a5c5acd03c77b9a2e55696
AdaZhou <your_120@126.com>
2014-04-16 17:52:54 +0800

    e

---
+++ MachineLearningAlgorithm/src/ada/ml/cluster/KMeans.java
@@ -0,0 +1,15 @@
+/**
+ * @author zhouxiaocao
+ * 2014年4月16日 下午5:50:40 create
+ */
+package ada.ml.cluster;
+
+/**
+ * @author zhouxc
+Input: Dataset D, number clusters k
+Output: Set of cluster representatives C, cluster membership vector m
+
+ */
+public class KMeans {
+
+}
